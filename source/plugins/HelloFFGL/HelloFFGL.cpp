46a41757e4e511d6addace7c065822af815e6c6f
Sem Schreuder <semschreuder@live.nl>
2019-04-10 19:36:36 +0200

    back to joris his init

--- source/plugins/HelloFFGL/HelloFFGL.cpp
+++ source/plugins/HelloFFGL/HelloFFGL.cpp
@@ -1,33 +1,53 @@
 #include "HelloFFGL.h"
 
-#define LayerDial1   0
-
-
+//here we define the word 'theFirstParam' to equal zero
+//this is handy so we don't have to remember which index each param has
+#define theFirstParam 0
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 //  Plugin information
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
 static CFFGLPluginInfo PluginInfo(
 	FFGLPlugin::CreateInstance,			// Create method
-	"0195",								// Plugin unique ID
-	"AKAI1",				        	// Plugin name
-	1,						   			// API major version number
+	"XXX1",								// Plugin unique ID
+	"Hello FFGL!",						// Plugin name
+	1,						   			// API major version number												
 	500,								// API minor version number
 	1,									// Plugin major version number
 	000,								// Plugin minor version number
 	FF_EFFECT,							// Plugin type
-	"AKAI1 CONTROLLER",		    	    // Plugin description
-	"Sem Schreuder // PixelClone"		// About
+	"Completely empty example",			// Plugin description
+	"Joris de Jong // Resolume"			// About 
 
+	/*The important bits here are:
+	- "Plugin unique ID". This number is used the identify the plugin internally and needs to be different for each and every FFGL plugin in existence.
+	Is there a single list where you can see all the IDs that are already in use? No. Feel free to organise one. 
+	My plugins start with HV and then have two numbers identifying them. So HV01 is my first plugin. Resolume's plugins start with Axxx
+	- "Plugin name". This is the name as its displayed in Resolume's effect list. 
+	- "Plugin type". Here you can let Resolume know whether the plugin is a source or effect. By putting FF_SOURCE you will create a source instead of an effect.
+	- "Plugin description" and "About" are shown in the Help panel in Resolume.
+	*/
 );
 
 FFGLPlugin::FFGLPlugin()
 	: CFreeFrameGLPlugin()
 {
-    SetParamInfo( LayerDial1, "LayerDial1", FF_TYPE_STANDARD, 0.f);
-    
+	/*This creates a parameter
+	Instead of using 0 in the beginning, we use our define 'theFirstParam'
+	"Does Nothing" is the name (16 character maximum)
+	FF_TYPE_STANDARD means it's a regular slider
+	and 0.5f is it's default value */
+	SetParamInfo( theFirstParam, "Does Nothing", FF_TYPE_STANDARD, 0.5f);
+
+	//because we set our param to be 0.5f by default, it makes sense that our float variable also starts off at 0.5f
+	aFloat = 0.5f;
+
+	/*This will make sure that the plugin gets recognised as an EFFECT on both OSX and Windows.
+	A SOURCE has 0 inputs
+	An EFFECT has 1 input
+	A TRANSITION has 2 inputs
+	So why is the definition there in the CreateInstance call. I don't know. Someone must have thought that was a good idea at the time */
 	SetMinInputs( 1 );
 	SetMaxInputs( 1 );
 }
@@ -37,12 +57,32 @@ FFGLPlugin::~FFGLPlugin()
 
 }
 
-
+/*ProcessOpenGL is like the draw() function in processing and openframeworks.
+Everything that happens in ProcessOpenGL happens everytime the plugin renders.*/
 FFResult FFGLPlugin::ProcessOpenGL(ProcessOpenGLStruct *pGL)
 {
+	//clear the background to a nice neutral grey
+	glClearColor( 0.23f, 0.23f, 0.23f, 1.0f );
+	glClear( GL_COLOR_BUFFER_BIT );
 
-    return FF_SUCCESS;
+	//set the color to Arena cyan
+	glColor4f( 0.5f, 1.0f, 0.82f, 1.0f );
+	
+	//now, we create a variable to hold the coordinates of the four corners of our rectangle
+	GLfloat verts[] =
+	{
+		-0.5f, 0.5f, //top left
+		0.5f, 0.5f, //top right
+		0.5f, -0.5f, //bottom right
+		-0.5f, -0.5f //bottom left
+	};
 
+	//and we draw those corners as a triangle fan
+	glEnableClientState( GL_VERTEX_ARRAY );
+	glVertexPointer( 2, GL_FLOAT, 0, verts );
+	glDrawArrays( GL_TRIANGLE_FAN, 0, 4 );
+
+	return FF_SUCCESS;
 }
 
 /* SetFloatParameter is called by Resolume when you change a slider in the effect */
@@ -51,9 +91,9 @@ FFResult FFGLPlugin::SetFloatParameter(unsigned int index, float value)
 	//now we use the callback to set our internal float variable, whenever the slider in Resolume is changed
 	switch (index)
 	{
-        case LayerDial1:
-            a01 = value; //this means we're setting our float to whatever value the first parameter has in Resolume
-            break;
+	case theFirstParam:
+		aFloat = value; //this means we're setting our float to whatever value the first parameter has in Resolume
+		break;
 	}
 
 	return FF_SUCCESS;
@@ -65,8 +105,15 @@ float FFGLPlugin::GetFloatParameter( unsigned int index )
 {
 	switch ( index )
 	{
-        case LayerDial1:
-            return a01; //right now, we just want to return the value of our float
-            break;
+	case theFirstParam:
+		return aFloat; //right now, we just want to return the value of our float
+		break;
+	default:
+		return 0.0f; //to be safe, always return something
+		break;
 	}
+
+	
 }
+
+
--- source/plugins/HelloFFGL/HelloFFGL.h
+++ source/plugins/HelloFFGL/HelloFFGL.h
@@ -18,75 +18,11 @@ public:
 	FFResult ProcessOpenGL(ProcessOpenGLStruct* pGL) override;
 	FFResult SetFloatParameter(unsigned int index, float value) override;
 	float GetFloatParameter( unsigned int index ) override;
-
+	
 
 	//this is a float that will be set by Resolume, and we can use it to change stuff in our plugin
 	//we're going to link the value of this float to the value of the slider in Resolume
-	float a01;
-    float a02;
-    float a03;
-    float a04;
-    float a05;
-    float a06;
-    float a07;
-    float a08;
-    float a09;
-    float a10;
-    float a11;
-    float a12;
-    float a13;
-    float a14;
-    float a15;
-    float a16;
-    float a17;
-    float a18;
-    float a19;
-    float a20;
-    float a21;
-    float a22;
-    float a23;
-    float a24;
-    float a25;
-    float a26;
-    float a27;
-    float a28;
-    float a29;
-    float a30;
-    float a31;
-    float a32;
-    float a33;
-    float a34;
-    float a35;
-    float a36;
-    float a37;
-    float a38;
-    float a39;
-    float a40;
-    float a41;
-    float a42;
-    float a43;
-    float a44;
-    float a45;
-    float a46;
-    float a47;
-    float a48;
-    float a49;
-    float a50;
-    float a51;
-    float a52;
-    float a53;
-    float a54;
-    float a55;
-    float a56;
-    float a57;
-    float a58;
-    float a59;
-    float a60;
-    float a61;
-    float a62;
-    float a63;
-    float a64;
-
+	float aFloat;
 
 	//this is boiler plate code that takes care of all the behind the scenes stuff used in creating a plugin
 	//in other words, we don't want to mess with this
