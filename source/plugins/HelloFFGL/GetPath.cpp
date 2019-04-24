00ddf92da3de5623441b5faedd96dca193f17b65
Sem Schreuder <semschreuder@live.nl>
2019-04-24 19:51:23 +0200

    start of windows ifdef path fix

--- source/plugins/HelloFFGL/GetPath.cpp
+++ source/plugins/HelloFFGL/GetPath.cpp
@@ -12,13 +12,26 @@
 string GetPath::getAssPath()
 {
     
-    string  HOME = getenv("HOME") ? getenv("HOME") : ".";
     string homePath;
+    
+#ifdef __APPLE__
+    string  HOME = getenv("HOME") ? getenv("HOME") : ".";
     string s1 = HOME;
     string s2 = "/Documents/Resolume Arena 6/Preferences/AdvancedOutput.xml";
     homePath = s1 + s2;
     
+#endif
     
+    
+#ifdef _WIN64
+    WCHAR path[MAX_PATH + 1];
+    if (SHGetSpecialFolderPathW(HWND_DESKTOP, path, CSIDL_DESKTOPDIRECTORY, FALSE))
+    {
+        std::wstring ws(path);
+        std::string HOME(ws.begin(), ws.end());
+        string s1 = HOME;
+    }
+#endif
     
     pugi::xml_document ass;
     pugi::xml_parse_result result = ass.load_file(homePath.c_str());
